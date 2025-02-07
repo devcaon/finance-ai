9de4103fc147cae6b441a96929dbfb0d17697cff
Claudio Oliveira <dev.caon@gmail.com>
2025-02-06 22:46:49 -0300

    feat: ensure transction and subscriptions  pages are protected

--- app/subscription/page.tsx
+++ app/subscription/page.tsx
@@ -1,6 +1,11 @@
+import { auth } from "@clerk/nextjs/server";
 import Navbar from "../_components/navbar";
+import { redirect } from "next/navigation";
 
 const Subscription = () => {
+  // verificar se tem usuário logado
+  const { userId } = auth();
+  if (!userId) redirect("/login");
   return <Navbar />;
 };
 
