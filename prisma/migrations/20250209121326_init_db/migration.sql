ef85f1bee4ea291d67a3bd4453ae7ccb4c72901a
Claudio Oliveira <dev.caon@gmail.com>
2025-02-09 17:09:03 -0300

    feat: add summary cards

--- prisma/migrations/20250209121326_init_db/migration.sql
+++ prisma/migrations/20250209121326_init_db/migration.sql
@@ -7,6 +7,9 @@ CREATE TYPE "TransactionCategory" AS ENUM ('HOUSING', 'TRANSPORTATION', 'FOOD', 
 -- CreateEnum
 CREATE TYPE "TransactionPaymentMethod" AS ENUM ('CREDIT_CARD', 'DEBIT_CARD', 'BANK_TRANSFER', 'BANK_SLIP', 'CASH', 'PIX', 'OTHER');
 
+-- CreateEnum
+CREATE TYPE "EmployeePosition" AS ENUM ('SEAMIST', 'ASSISTANT');
+
 -- CreateTable
 CREATE TABLE "Transaction" (
     "id" TEXT NOT NULL,
@@ -18,6 +21,22 @@ CREATE TABLE "Transaction" (
     "date" TIMESTAMP(3) NOT NULL,
     "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "updatedAt" TIMESTAMP(3) NOT NULL,
+    "userId" TEXT NOT NULL,
 
     CONSTRAINT "Transaction_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Employee" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "salary" DECIMAL(10,2) NOT NULL,
+    "position" "EmployeePosition" NOT NULL,
+    "dateStart" TIMESTAMP(3) NOT NULL,
+    "dateEnd" TIMESTAMP(3) NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+    "userId" TEXT NOT NULL,
+
+    CONSTRAINT "Employee_pkey" PRIMARY KEY ("id")
 );
