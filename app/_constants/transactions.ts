6c6b1c8032bdfb825092f9bde5b72d9e229933f1
Claudio Oliveira <dev.caon@gmail.com>
2025-02-10 21:11:54 -0300

    feat: add last transactions component

--- app/_constants/transactions.ts
+++ app/_constants/transactions.ts
@@ -31,6 +31,16 @@ export const TRANSACTION_PAYMENT_METHOD_LABELS = {
   OTHER: "Outro",
 };
 
+export const TRANSACTION_PAYMENT_METHOD_ICONS = {
+  [TransactionPaymentMethod.CREDIT_CARD]: "credit-card.svg",
+  [TransactionPaymentMethod.DEBIT_CARD]: "debit-card.svg",
+  [TransactionPaymentMethod.BANK_TRANSFER]: "bank-transfer.svg",
+  [TransactionPaymentMethod.BANK_SLIP]: "bank-slip.svg",
+  [TransactionPaymentMethod.CASH]: "money.svg",
+  [TransactionPaymentMethod.PIX]: "pix.svg",
+  [TransactionPaymentMethod.OTHER]: "other.svg",
+};
+
 export const TRANSACTION_TYPE_OPTIONS = [
   {
     value: TransactionType.DEPOSIT,
@@ -113,6 +123,10 @@ export const TRANSACTION_CATEGORY_OPTIONS = [
   {
     value: TransactionCategory.PRODUCTION,
     label: TRANSACTION_CATEGORY_LABELS[TransactionCategory.PRODUCTION],
+  },
+  {
+    value: TransactionCategory.SALE,
+    label: TRANSACTION_CATEGORY_LABELS[TransactionCategory.SALE],
   },
   {
     value: TransactionCategory.PURCHASE_OF_EQUIPMENT,
--- app/_data/get-dashboard/index.ts
+++ app/_data/get-dashboard/index.ts
@@ -85,6 +85,12 @@ export const getDashboard = async ({ month, year }: GetDashboardProps) => {
     ),
   }));
 
+  const lastTransactions = await db.transaction.findMany({
+    where,
+    orderBy: { date: "desc" },
+    take: 10,
+  });
+
   return {
     balance,
     depositsTotal,
@@ -92,5 +98,6 @@ export const getDashboard = async ({ month, year }: GetDashboardProps) => {
     expensesTotal,
     typesPercentage,
     totalExpensePerCategory,
+    lastTransactions,
   };
 };
