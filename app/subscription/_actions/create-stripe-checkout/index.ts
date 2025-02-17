8bd4de58df6d9d9782487e1dedf7f3c47834850b
Claudio OLiveira <dev.caon@gmail.com>
2025-02-16 22:37:26 -0300

    fix: new fixed apiversion stripe

--- app/subscription/_actions/create-stripe-checkout/index.ts
+++ app/subscription/_actions/create-stripe-checkout/index.ts
@@ -14,7 +14,7 @@ export const createStripeCheckout = async () => {
   }
 
   const stripe = new Stripe(process.env.STRIPE_SECRET_KEY, {
-    apiVersion: "2024-10-28.acacia",
+    apiVersion: "2025-01-27.acacia",
   });
 
   const session = await stripe.checkout.sessions.create({
